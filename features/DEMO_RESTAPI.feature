c284410dafbec028ffaef7d6cf5a3265ae4486b6
LayMui <mui_ume@hotmail.com>
2021-09-07 12:23:33 +0800

    Add delete request

--- features/DEMO_RESTAPI.feature
+++ features/DEMO_RESTAPI.feature
@@ -1,15 +1,15 @@
 Feature: DEMO_RESTAPI
-
+  In order to perform CRUD on message 
+    As a API developer James
+    James wants to create, update, get, delete a new message
 
   Background:
     Given James is at the base url
 
   Rule: create new message
-    @test
+  
+  @test
   Scenario Outline: create a new message 
-    In order to create a new message using POST
-    As a API developer James
-    James wants to create a new message
     When he wants to create a new message with author <author> and message <message>
     Then he is able to create the new message author <author> and message <message>
     Examples:
@@ -17,10 +17,42 @@ Feature: DEMO_RESTAPI
       | "Curry Blake" | "Speaking in tongue" | 
      
   
-  @test1
-  Scenario: get a single message (uid 4a386f39-a333-4b30-81da-ae1be974db69)
+  Scenario: get a single message 
     In order to get a single message
     As a API developer James
     James wants to get a single message
-    When James want to get a single message
+    When he want to get a single message
     Then he is able to get a single message
+
+
+  Scenario: get all messages 
+    In order to view all messages 
+    As a API developer James
+    James wants to get all messages to be displayed
+    When he view all messages
+    Then he is able to view all the messages
+
+
+ Scenario Outline: update a message 
+    In order to update a message
+    As a API developer James
+    James wants to update a message
+    When he update a message with author <author> and <message>
+    Then he see the message get updated
+
+    Examples:
+      | author                |    message       | 
+      | "john smart ferguson" |  "BDD in Action" |  
+
+@test
+  Scenario Outline: delete a message 
+    In order to delete a message
+    As a API developer James
+    James wants to able to delete a message
+    When he delete a message <message>
+    Then he is able to delete the message
+
+    Examples:
+      | message | 
+      | "1" | 
+
--- src/step_definitions/DEMO_RESTAPI.steps.ts
+++ src/step_definitions/DEMO_RESTAPI.steps.ts
@@ -1,7 +1,7 @@
 import { DataTable, Given, Then, When } from '@cucumber/cucumber';
 import { and, Ensure, equals } from '@serenity-js/assertions';
 import { Actor, List, Log, Property } from '@serenity-js/core';
-import { CallAnApi, GetRequest, LastResponse, PostRequest, Send } from '@serenity-js/rest';
+import { CallAnApi, DeleteRequest, GetRequest, LastResponse, PostRequest, PutRequest, Send } from '@serenity-js/rest';
 import MessageDto from '../dto/messageDto';
 
 
@@ -29,14 +29,51 @@ actor.attemptsTo(
 ));
 
 
-// When('{actor} want to get a single message', (actor: Actor) => 
-// actor.attemptsTo(
-//     Send.a(GetRequest.to('/taqelah/messages/2')),
-//     Log.the(LastResponse.body()),
-// ));
+When('{pronoun} want to get a single message', (actor: Actor) => 
+actor.attemptsTo(
+    Send.a(GetRequest.to('/taqelah/messages/2')),
+    Log.the(LastResponse.body()),
+));
 
 
-// Then('{pronoun} is able to get a single message', (actor: Actor) =>
-// actor.attemptsTo(
-//     Ensure.that(LastResponse.status(), equals(200)),
-// ));
+Then('{pronoun} is able to get a single message', (actor: Actor) =>
+actor.attemptsTo(
+    Ensure.that(LastResponse.status(), equals(200)),
+));
+
+When('{pronoun} view all messages', (actor: Actor) => 
+actor.attemptsTo(
+    Send.a(GetRequest.to('/taqelah/messages')),
+    Log.the(LastResponse.body()),
+));
+
+Then('{pronoun} is able to view all the messages', (actor: Actor) =>
+actor.attemptsTo(
+    Ensure.that(LastResponse.status(), equals(200)),
+));
+
+
+When('{pronoun} update a message with author {string} and {string}', (actor: Actor, author: string, message: string) =>
+    actor.attemptsTo(
+        Send.a(PutRequest.to('/taqelah/messages/2').with({ author: author, message: message })),
+        Log.the(LastResponse.body()),
+    ));
+
+
+Then('{pronoun} see the message get updated', (actor: Actor) =>
+actor.attemptsTo(
+    Ensure.that(LastResponse.status(), equals(200)),
+));
+
+   
+When('{pronoun} delete a message {string}', (actor: Actor, message: string) =>
+    actor.attemptsTo(
+        Send.a(DeleteRequest.to('/taqelah/messages/' + message)),
+        Log.the(LastResponse.body()),
+    ));
+
+Then('{pronoun} is able to delete the message', (actor: Actor) =>
+    actor.attemptsTo(
+        Ensure.that(LastResponse.status(), equals(200)),
+    ));
+    
